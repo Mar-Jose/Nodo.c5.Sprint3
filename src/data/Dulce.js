b0539d072e983b9fa89c828a38eabc7b0661c7b5
Mar-Jose <de.la.taes@gmail.com>
2026-09-15 22:44:24 -0300

    agregue stock en arreglo de dulces de carpeta data

--- src/data/Dulce.js
+++ src/data/Dulce.js
@@ -1,4 +1,4 @@
-﻿// Catálogo de Dulces Regionales de Catamarca
+﻿
 export const dulcesCatamarca = [
   {
     id: 1,
@@ -6,6 +6,7 @@ export const dulcesCatamarca = [
     categoria: "Confitería",
     origen: "Belén",
     precio: 4500,
+    stock: 12,
     puntaje: 4.9,
     fechaReceta: 1890,
     requiereRefrigeracion: false,
@@ -16,6 +17,7 @@ export const dulcesCatamarca = [
     categoria: "Turrón",
     origen: "Andalgalá",
     precio: 5200,
+    stock: 8,
     puntaje: 4.8,
     fechaReceta: 1920,
     requiereRefrigeracion: false
@@ -26,6 +28,7 @@ export const dulcesCatamarca = [
     categoria: "Dulce en Almíbar",
     origen: "Santa María",
     precio: 3800,
+    stock: 15,
     puntaje: 4.9,
     fechaReceta: 1885,
     requiereRefrigeracion: true
@@ -36,6 +39,7 @@ export const dulcesCatamarca = [
     categoria: "Arrope",
     origen: "Tinogasta",
     precio: 2900,
+    stock: 20,
     puntaje: 4.6,
     fechaReceta: 1850,
     requiereRefrigeracion: false
@@ -46,6 +50,7 @@ export const dulcesCatamarca = [
     categoria: "Alfajores y Colaciones",
     origen: "Belén",
     precio: 6000,
+    stock: 6,
     puntaje: 5.0,
     fechaReceta: 1935,
     requiereRefrigeracion: false
@@ -56,6 +61,7 @@ export const dulcesCatamarca = [
     categoria: "Panificación Dulce",
     origen: "Capital",
     precio: 1800,
+    stock: 24,
     puntaje: 4.7,
     fechaReceta: 1910,
     requiereRefrigeracion: false
@@ -66,6 +72,7 @@ export const dulcesCatamarca = [
     categoria: "Panificación Dulce",
     origen: "Santa María",
     precio: 1200,
+    stock: 30,
     puntaje: 4.8,
     fechaReceta: 1895,
     requiereRefrigeracion: true
@@ -76,6 +83,7 @@ export const dulcesCatamarca = [
     categoria: "Dulce en Pan",
     origen: "Andalgalá",
     precio: 2500,
+    stock: 18,
     puntaje: 4.5,
     fechaReceta: 1905,
     requiereRefrigeracion: false
@@ -86,6 +94,7 @@ export const dulcesCatamarca = [
     categoria: "Arrope",
     origen: "Pomán",
     precio: 2700,
+    stock: 14,
     puntaje: 4.4,
     fechaReceta: 1860,
     requiereRefrigeracion: false
@@ -96,6 +105,7 @@ export const dulcesCatamarca = [
     categoria: "Confitería",
     origen: "Recreo",
     precio: 4100,
+    stock: 10,
     puntaje: 4.6,
     fechaReceta: 1950,
     requiereRefrigeracion: false
@@ -106,6 +116,7 @@ export const dulcesCatamarca = [
     categoria: "Alfajores y Colaciones",
     origen: "Fray Mamerto Esquiú",
     precio: 1500,
+    stock: 25,
     puntaje: 4.9,
     fechaReceta: 1975,
     requiereRefrigeracion: true
@@ -116,6 +127,7 @@ export const dulcesCatamarca = [
     categoria: "Confitería",
     origen: "Tinogasta",
     precio: 3200,
+    stock: 9,
     puntaje: 4.3,
     fechaReceta: 1840,
     requiereRefrigeracion: false
@@ -126,6 +138,7 @@ export const dulcesCatamarca = [
     categoria: "Dulce Untable",
     origen: "Valle Viejo",
     precio: 3600,
+    stock: 11,
     puntaje: 4.8,
     fechaReceta: 1960,
     requiereRefrigeracion: true
@@ -136,6 +149,7 @@ export const dulcesCatamarca = [
     categoria: "Arrope",
     origen: "Capayán",
     precio: 3100,
+    stock: 13,
     puntaje: 4.5,
     fechaReceta: 1870,
     requiereRefrigeracion: false
@@ -146,6 +160,7 @@ export const dulcesCatamarca = [
     categoria: "Dulce en Almíbar",
     origen: "Andalgalá",
     precio: 3400,
+    stock: 7,
     puntaje: 4.7,
     fechaReceta: 1880,
     requiereRefrigeracion: true
@@ -156,6 +171,7 @@ export const dulcesCatamarca = [
     categoria: "Panificación Dulce",
     origen: "El Alto",
     precio: 2200,
+    stock: 22,
     puntaje: 4.4,
     fechaReceta: 1940,
     requiereRefrigeracion: false
@@ -166,6 +182,7 @@ export const dulcesCatamarca = [
     categoria: "Dulce en Almíbar",
     origen: "Paclín",
     precio: 3900,
+    stock: 10,
     puntaje: 4.9,
     fechaReceta: 1892,
     requiereRefrigeracion: true
@@ -176,6 +193,7 @@ export const dulcesCatamarca = [
     categoria: "Arrope",
     origen: "Tinogasta",
     precio: 3000,
+    stock: 16,
     puntaje: 4.5,
     fechaReceta: 1865,
     requiereRefrigeracion: false
@@ -186,6 +204,7 @@ export const dulcesCatamarca = [
     categoria: "Confitería",
     origen: "Belén",
     precio: 4700,
+    stock: 8,
     puntaje: 4.8,
     fechaReceta: 1900,
     requiereRefrigeracion: false
@@ -196,6 +215,7 @@ export const dulcesCatamarca = [
     categoria: "Turrón",
     origen: "Santa María",
     precio: 5500,
+    stock: 5,
     puntaje: 4.7,
     fechaReceta: 1930,
     requiereRefrigeracion: false
@@ -206,6 +226,7 @@ export const dulcesCatamarca = [
     categoria: "Dulce en Pan",
     origen: "Londres",
     precio: 2600,
+    stock: 17,
     puntaje: 4.7,
     fechaReceta: 1898,
     requiereRefrigeracion: false
@@ -216,6 +237,7 @@ export const dulcesCatamarca = [
     categoria: "Dulce Untable",
     origen: "Ambato",
     precio: 3300,
+    stock: 12,
     puntaje: 4.6,
     fechaReceta: 1902,
     requiereRefrigeracion: true
@@ -226,6 +248,7 @@ export const dulcesCatamarca = [
     categoria: "Dulce en Almíbar",
     origen: "Capayán",
     precio: 3500,
+    stock: 9,
     puntaje: 4.8,
     fechaReceta: 1915,
     requiereRefrigeracion: true
@@ -236,6 +259,7 @@ export const dulcesCatamarca = [
     categoria: "Dulce Untable",
     origen: "Valle Viejo",
     precio: 2800,
+    stock: 19,
     puntaje: 5.0,
     fechaReceta: 1955,
     requiereRefrigeracion: true
@@ -246,6 +270,7 @@ export const dulcesCatamarca = [
     categoria: "Gaznate",
     origen: "Fray Mamerto Esquiú",
     precio: 2000,
+    stock: 21,
     puntaje: 4.9,
     fechaReceta: 1965,
     requiereRefrigeracion: true
